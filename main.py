9f654a8f5256cda083098d7bee065eb1944ab7a9
EvgenyPHP <kalinichenko.online@gmail.com>
2025-06-27 19:09:03 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, FSInputFile
@@ -12,14 +11,19 @@ import logging
 import asyncio
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN, default=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)
 
+
 class Form(StatesGroup):
     goal = State()
     done = State()
+
 
 @dp.message(F.text == "/start")
 async def cmd_start(message: Message, state: FSMContext):
@@ -36,10 +40,12 @@ async def cmd_start(message: Message, state: FSMContext):
     )
     await state.set_state(Form.goal)
 
+
 @dp.message(Form.goal, F.text.in_(["Похудение", "Поддержание", "Набор массы"]))
 async def process_goal(message: Message, state: FSMContext):
     goal = message.text
-    # Демоданные (обычно рассчитываются по формулам и анкетным данным)
+
+    # Демоданные
     result = {
         "Похудение": {"calories": 1424, "protein": 74, "fat": 49, "carbs": 172},
         "Поддержание": {"calories": 1624, "protein": 74, "fat": 49, "carbs": 222},
@@ -47,30 +53,25 @@ async def process_goal(message: Message, state: FSMContext):
     }
 
     await state.update_data(result=result[goal])
-
     res = result[goal]
-    text = (
-        f"""<b>Твой результат:</b>
-"
-        f"Калории: <b>{res['calories']} ккал</b>
-"
-        f"Белки: {res['protein']} г
-"
-        f"Жиры: {res['fat']} г
-"
-        f"Углеводы: {res['carbs']} г"""
-    )
+
+    text = f"""<b>Твой результат:</b>
+Калории: <b>{res['calories']} ккал</b>
+Белки: {res['protein']} г
+Жиры: {res['fat']} г
+Углеводы: {res['carbs']} г"""
+
     await message.answer(text)
 
-    await state.set_state(Form.done)
-
-    # Отображаем финальные кнопки
+    # Показываем финальные кнопки
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Сохранить результаты в PDF", callback_data="save_pdf")],
         [InlineKeyboardButton(text="Подписаться на канал", url="https://t.me/doc_kalinichenko")],
         [InlineKeyboardButton(text="Начать заново", callback_data="restart")]
     ])
     await message.answer("Что хочешь сделать дальше?", reply_markup=keyboard)
+    await state.set_state(Form.done)
+
 
 @dp.callback_query(F.data == "save_pdf")
 async def save_pdf(callback: CallbackQuery, state: FSMContext):
@@ -95,11 +96,13 @@ async def save_pdf(callback: CallbackQuery, state: FSMContext):
     os.remove(file_path)
     await callback.answer()
 
+
 @dp.callback_query(F.data == "restart")
 async def restart(callback: CallbackQuery, state: FSMContext):
     await state.clear()
     await cmd_start(callback.message, state)
     await callback.answer()
+
 
 async def main():
     await dp.start_polling(bot)
